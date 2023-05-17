4b71e38fc43c3e160ea1f9ec025ee72b8dce319b
Jules Renck <julesrenck@gmail.com>
2023-05-17 17:26:43 -0300

    Desenvolvido layout da tela de register

--- frontend/authentication-app-vue/src/components/auth-components/register/Register.js
+++ frontend/authentication-app-vue/src/components/auth-components/register/Register.js
@@ -2,7 +2,16 @@ export default {
   name: 'RegisterComponent',
   data() {
     return {
-
+      registerForm: {
+        name: null,
+        email: null,
+        password: null,
+      },
     };
   },
+  methods: {
+    registerSubmitUserForm() {},
+
+    async submitRegisterUser() {}
+  }  
 };
